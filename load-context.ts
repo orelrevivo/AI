8f17d8bdf388ffe874dfe777190e8c340accf3f5
Orel Revivo <orelrevivo3999@gmail.com>
2026-03-25 00:04:12 +0200

    Completely disable Cloudflare files and clean up environment

--- load-context.ts
+++ load-context.ts
@@ -1,9 +1,2 @@
-import { type PlatformProxy } from 'wrangler';
-
-type Cloudflare = Omit<PlatformProxy<Env>, 'dispose'>;
-
-declare module '@remix-run/cloudflare' {
-  interface AppLoadContext {
-    cloudflare: Cloudflare;
-  }
-}
+// DEPRECATED FOR VERCEL
+export {};
--- worker-configuration.d.ts
+++ worker-configuration.d.ts
@@ -1,7 +1,2 @@
-interface Env {
-  ANTHROPIC_API_KEY: string;
-  GLM_API_KEY: string;
-  ZAI_API_KEY: string;
-}
-
-
+// DEPRECATED FOR VERCEL
+export {};
