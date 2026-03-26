a7c24125329fed3fc397da84b180a58467c0f7c3
Orel Revivo <orelrevivo3999@gmail.com>
2026-03-26 02:47:45 +0200

    add

--- functions/[[path]].ts
+++
@@ -1,4 +0,0 @@
-// DEPRECATED FOR VERCEL
-// This file is no longer used since we've migrated to Vercel Node.js.
-// You can safely delete the 'functions' directory.
-export const onRequest = () => new Response('Deprecated', { status: 404 });
